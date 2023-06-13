62a759bfde8e9dee3412ff94e681f944d65e040a
helmi77 <helmi77@outlook.com>
2023-06-13 21:06:34 +0200

    Fix currency casing

--- forex.py
+++ forex.py
@@ -11,6 +11,8 @@ async def get_rate(from_symbol, to_symbol, session):
         return rates[key]
 
     api_key = os.environ.get("FOREX_API_KEY")
+    from_symbol = from_symbol.upper()
+    to_symbol = to_symbol.upper()
     url = f"https://marketdata.tradermade.com/api/v1/convert?api_key={api_key}&from={from_symbol}&to={to_symbol}&amount=1"
     async with session.get(url) as resp:
         text = await resp.text()
